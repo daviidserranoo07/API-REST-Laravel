a1fe5547b03b366a2d7584529b2f8682ff63ace7
David Serrano Domínguez <daviidserranoo07@gmail.com>
2024-08-10 09:22:58 +0200

    Añadimos clase authController

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\User\PostRequest;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use \stdClass;
+
+class AuthController extends Controller
+{
+    public function register(PostRequest $request){
+
+        $data = $request->validated();
+
+        $user = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password']),
+        ]);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'status' => true,
+            'data' => $user,
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+        ],201);
+    }
+}
